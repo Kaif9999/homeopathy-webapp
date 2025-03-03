413d445d7d2df23efde1a3abf75654803e556b3e
= <kaifmohd5000@gmail.com>
2025-03-03 18:14:05 +0530

    mobile responsiveness

--- components/about.tsx
+++ components/about.tsx
@@ -21,39 +21,39 @@ export const About = () => {
               Lab Tests Available
             </span>
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-2 gap-8">
-            <div className="w-auto h-auto p-6 shadow-lg rounded-2xl bg-blue-900/50 backdrop-blur-sm">
-              <h2 className="text-3xl font-semibold text-white mb-4 text-center">
-                About Us
-              </h2>
-              <p className="text-lg text-gray-200 mb-4 leading-relaxed">
-                Publichomepathy clinic is your place to be if you want to gain a
-                new perspective on your life and introspect to find the real
-                you. Our team of board-certified and experienced professionals,
-                comprising homeopathic doctors and practitioners, naturopathy
-                experts, and lifestyle transformation professionals, will assist
-                in diagnosing all the medical ailments, ranging from mental and
-                physical to social, and assist you in alleviating and resolving
-                them from the roots.
-              </p>
-              <p className="text-lg text-gray-200 leading-relaxed">
-                Serving society for generations, we are known to deliver
-                compassionate and personalised care to our patients to overcome
-                all the hurdles holding them to live their lives to the fullest.
-                Cure Wellness Centre, a homeopathy and naturopathy centre, has
-                access to all the latest advancements in the field and
-                evidence-based practices to deliver the best possible patient
-                outcomes.
-              </p>
-            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="w-auto h-auto p-4 shadow-lg rounded-2xl bg-emerald-900/50 backdrop-blur-sm">
               <img
-                src="about_image.jpeg"
-                alt="About"
-                className="w-full h-full object-cover rounded-2xl"
+              src="about_image.jpeg"
+              alt="About"
+              className="w-full h-full object-cover rounded-2xl"
               />
             </div>
-          </div>
+            <div className="w-auto h-auto p-6 shadow-lg rounded-2xl bg-blue-900/50 backdrop-blur-sm">
+              <h2 className="text-3xl font-semibold text-white mb-4 text-center">
+              About Us
+              </h2>
+              <p className="text-lg text-gray-200 mb-4 leading-relaxed">
+              Publichomepathy clinic is your place to be if you want to gain a
+              new perspective on your life and introspect to find the real
+              you. Our team of board-certified and experienced professionals,
+              comprising homeopathic doctors and practitioners, naturopathy
+              experts, and lifestyle transformation professionals, will assist
+              in diagnosing all the medical ailments, ranging from mental and
+              physical to social, and assist you in alleviating and resolving
+              them from the roots.
+              </p>
+              <p className="text-lg text-gray-200 leading-relaxed">
+              Serving society for generations, we are known to deliver
+              compassionate and personalised care to our patients to overcome
+              all the hurdles holding them to live their lives to the fullest.
+              Cure Wellness Centre, a homeopathy and naturopathy centre, has
+              access to all the latest advancements in the field and
+              evidence-based practices to deliver the best possible patient
+              outcomes.
+              </p>
+            </div>
+            </div>
         </div>
       </section>
     </div>
